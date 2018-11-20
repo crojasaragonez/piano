9e7c723c5e86b091e1fa603902f5c74984a3f974
Carlos Luis Rojas Aragonés <crojasaragonez@hotmail.com>
2018-11-19 20:08:08 -0600

    simplify code

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,17 @@
 var synth = new Tone.AMSynth().toMaster();
 var worker  = new Worker('/js/worker.js');
 
-worker.onmessage = function(event){
+worker.onmessage = function(event) {
   var msg = event.data;
   synth.triggerAttackRelease(msg.note, "8n");
 
-  $('#' + msg.note).queue(function (next) {
-    $(this).css('background-color', msg.color);
-    next();
-  }).delay(500).queue(function (next) {
-    $(this).css('background-color', '');
-    next();
-  });
+  $('#' + msg.note).css('background-color', msg.color)
+                   .delay(500)
+                   .queue(function () {
+                      $(this).css('background-color', '').dequeue();
+                    });
 };
 
-$('.key').click(function(event) {
+$('.key').click(function() {
   $.post('/', { note: $(this).attr('id'), color: $('#color').val() });
 });
--- public/js/worker.js
+++ public/js/worker.js
@@ -1,6 +1,5 @@
-var thisp     = this;
 var es  = new EventSource('/stream');
 es.onmessage = function(e) {
   var msg = JSON.parse(e.data);
-  thisp.postMessage(msg);
+  self.postMessage(msg);
 };
